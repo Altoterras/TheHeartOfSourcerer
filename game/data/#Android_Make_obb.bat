ea9e330c8d801b1b420bca2971c153bda9a1f35c
FOURNETSHA\r-kishi <r-kishi@sigrest.co.jp>
2017-07-11 14:23:24 +0900

    Android Studio 対応中（途中）…

---
+++ game/data/#Android_Make_obb.bat
@@ -0,0 +1,4 @@
+%~d0
+cd %~dp0
+D:\Systems\Android\Sdk\tools\jobb.bat -pn jp.co.altoterras.sourcerer01 -pv 1 -d D:\Works\Projects\Enlight\Src_Public\game\data\sourcerer_obb -o D:\Works\Projects\Enlight\Src_Public\game\data\main.100.jp.co.altoterras.sourcerer01.obb
+pause
